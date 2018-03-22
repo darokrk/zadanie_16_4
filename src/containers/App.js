e7ebd624e4f15cdb09e46b3c1af57b7eafae1066
Dariusz Rosiek <daro.krk@interia.pl>
2018-03-22 18:36:24 +0100

    bugs fixed

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     render() {
         return (
             <div className={style.TodoApp}>
-
+                Tutaj pojawią się komponenty naszej aplikacji
             </div>
         )
     }
